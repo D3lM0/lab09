ab90381825849b27126e0792673ced5e3a3d8058
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-11-18 09:41:12 +0100

    Implemented SimpleGUI class

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -6,6 +6,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -16,6 +19,7 @@ public final class SimpleGUI {
     private static final String TITLE = "File Simple GUI";
     private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame(TITLE);
+    private Controller controller;
 
     public SimpleGUI() {
         final JPanel canvas = new JPanel();
@@ -27,6 +31,20 @@ public final class SimpleGUI {
 
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        save.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                controller = new Controller();
+                try {
+                    controller.saveOnFile(text.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace(); // NOPMD
+                }
+            }
+
+        });
     }
 
     private void display() {
